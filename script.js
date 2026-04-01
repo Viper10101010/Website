492c13292c841a0b0cf11201f1b6c9c058f0f324
Viper10101010 <seashells953@gmail.com>
2026-04-01 08:09:07 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("Elbanna Web Design Loaded");
+
+// Smooth scroll navigation
+document.querySelectorAll('nav a').forEach(anchor => {
+anchor.addEventListener('click', function(e){
+e.preventDefault();
+document.querySelector(this.getAttribute('href'))
+.scrollIntoView({behavior:'smooth'});
+});
+});
